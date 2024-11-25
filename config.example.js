48f664340db8f850c52ef95ca2f5a88d5cc5bf31
Carlos Carvajal <carv0066@algonquinlive.com>
2024-11-25 03:20:44 -0500

    changes

--- config.example.js
+++
@@ -1,8 +0,0 @@
-// config.example.js
-const config = {
-    EMAILJS_PUBLIC_KEY: 'YOUR_PUBLIC_KEY_HERE',
-    SERVICE_ID: 'YOUR_SERVICE_ID_HERE',
-    TEMPLATE_ID: 'YOUR_TEMPLATE_ID_HERE'
-};
-
-export default config;
